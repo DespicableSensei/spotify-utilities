b2238bfd0e675ee517343aed7ec00ffbd7af65d2
Emre Öztürk <emreozturka@gmail.com>
2018-08-07 14:28:06 +0300

    clean up and cover page

--- safariDropdown.js
+++
@@ -1,7 +0,0 @@
-var selectOption = document.querySelectorAll('#slct option');
-for (var i = 0; i < selectOption.length; i++) {
-  var item = selectOption[i];
-  if (item.innerHTML === "") {
-    item.remove();
-  };
-}
